cfd90c6964d861195b5c997ef88fb7e143f87df3
tranvanhoanghai <74179524+tranvanhoanghai@users.noreply.github.com>
2021-11-29 07:44:31 +0700

    add demo

---
+++ demo/demo_app/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-button v-b-toggle.sidebar-backdrop>Toggle Sidebar</b-button>
+    <Sidebar></Sidebar>
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import Sidebar from '@/components/SideBar.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+    Sidebar,
+  }
+}
+</script>
