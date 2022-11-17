65191a9a245c76846ef0c79cc77eff0365547d16
Hai Tran <tranvanhoanghai2510@gmail.com>
2022-11-17 20:47:32 +0700

    remove

--- app-vue-ts/src/i18n/index.ts
+++
@@ -1,7 +0,0 @@
-import enUS from './en-US';
-import vi from './vi';
-
-export default {
-    'en-US': enUS,
-    'vi': vi,
-};
--- app-vue-ts/src/i18n/vi/index.ts
+++
@@ -1,4 +0,0 @@
-export default {
-    failed: 'Hành động thất bại',
-    success: 'Hành động thành công'
-};
